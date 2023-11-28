f32c021459ce64e0fcfcc6226fc9569f08f74d3f
Youngdev111 <152314631+Youngdev111@users.noreply.github.com>
2023-11-28 09:04:48 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//counter app fuunctionality
+
+/*
+objective  is a collection of related data.
+properties & function
+*/
+
+//Array Declaration
+let mycarArr = ["Bmw","benz","Toyota","chevrolet","ford"];
+
+let studentname ="joseph";
